bf9e1407bdde2b5706d414c788b5787b82524611
gunishmatta <gunishmatta@gmail.com>
2019-11-15 17:43:38 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# chattter
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/chattter)
---
+++ index.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Hello from './Hello';
+import './style.css';
+import Chatkit from '@pusher/chatkit'
+import MessageList from './components/MessageList'
+import SendMessageForm from './components/SendMessageForm'
+import RoomList from './components/RoomList'
+import NewRoomForm from './components/NewRoomForm'
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'React'
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Hello name={this.state.name} />
+        <p>
+          Start editing to see some magic happen :)
+        </p>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
